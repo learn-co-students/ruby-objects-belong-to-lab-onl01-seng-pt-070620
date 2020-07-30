dff9ad3165276b8ce3aad7af13c1a01f22beae4b
github username <jhernandez1692@gmail.com>
2020-07-30 14:00:40 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,10 @@
+class Author
+
+    attr_accessor :name
+
+    def name (name = "Uncle Bob")
+        @name = name
+    end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+
+    attr_accessor :title , :artist
+
+    def initialize(title = "7/11")
+        @title = title
+        @artist = artist
+    end
+
+
+end
